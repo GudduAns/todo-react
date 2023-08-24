f202b45be0e9f6f30f6863dd28f69b0bab7bd849
Guddu Ansari <guddu.edneed@gmail.com>
2023-08-24 17:53:59 +0530

    make search bugs

--- src/Components/SearchTool/SearchTool.jsx
+++ src/Components/SearchTool/SearchTool.jsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import data from './data.json'
 const SearchTool = () => {
-    const inputStyle={
-        background:'transparent',
-        outline:'none',
-        border:'none',
-        borderBottom:'1px solid #202020',
-        color:'#ffffff73'
+    const inputStyle = {
+        background: 'transparent',
+        outline: 'none',
+        border: 'none',
+        borderBottom: '1px solid #ffffff73',
+        color: '#ffffff73'
     }
+    const [inputVal, setInputval] = useState('')
+
+    const handleChange = (e) => {
+        setInputval(e.target.value)
+    }
+
     return (
-        <>
-            <input type="text" name="" id="" className="my-3"  style={inputStyle}/>
+        <div className="container my-5">
+            <input
+                type="text"
+                className="my-3"
+                style={inputStyle}
+                value={inputVal}
+                onChange={handleChange}
+            />
             <br />
-            {
-                data.data.map((val,key)=>{
+            {   
+               data.data.filter(item=>item.name.toLowerCase().includes(inputVal.toLowerCase()))
+               .map((val, key) => {
                     return (
-                        <p style={{'color':'#ffffff73'}}>{val.name}</p>
+                        <p style={{ 'color': '#ffffff73' }} key={key}>{val.name}</p>
                     )
                 })
             }
-        </>
+        </div>
     )
 }
 
