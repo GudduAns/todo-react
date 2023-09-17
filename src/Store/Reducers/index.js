25ac392601173f06bfb8c4b6a3f00959495a452e
Gd-ans <ansariguddu1020@gmail.com>
2023-09-17 12:58:20 +0530

    wrking on redux

--- src/Store/Reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import { UpDownReducer } from './UpDownReducer'
-export const RootReducer = () => combineReducers({
-    UpDownReducer,
-})
--- src/Store/index.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import { RootReducer } from "./Reducers/index";
-
-export const Store = createStore(RootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
